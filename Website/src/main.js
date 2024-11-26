783e4c93eafdd92372e46cf3395a9441b817094b
unknown <60006290+ethmacc@users.noreply.github.com>
2024-11-26 19:46:10 +0000

    Add map and visualise lsoa geojson, add vite

---
+++ Website/src/main.js
@@ -0,0 +1,41 @@
+import '../front-end/css/style-app.css'
+import { BASEMAP } from '@deck.gl/carto';
+import { Map } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+import { MapboxOverlay } from '@deck.gl/mapbox';
+import { ScatterplotLayer, GeoJsonLayer } from '@deck.gl/layers';
+
+//Initialise Maplibre BaseMap
+const map = new Map({
+  container: 'map',
+  style: BASEMAP.POSITRON,
+  interactive: true,
+  center:[-0.12262486445294093,51.50756471490389],
+  zoom: 12
+})
+
+//wait until map is loaded before loading data
+await map.once('load');
+
+//set lsoa geojson
+const LSOA21 = '../data_processing/lsoa21.geojson'
+
+const deckOverlay = new MapboxOverlay({
+    interleaved: true,
+    layers: [
+        new GeoJsonLayer({
+            id: 'lsoa-map', 
+            data: LSOA21, 
+            // Styles
+            stroked: true,
+            filled: false,
+            lineWidthMinPixels: 1,
+            opacity: 0.5,
+            getLineColor: [252, 148, 3],
+            beforeId: 'place_suburbs' 
+          }),
+    ]
+});
+
+map.addControl(deckOverlay);
