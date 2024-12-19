7e48c23490db5fb62ff7ecc93e4cc05e0e2a4618
unknown <60006290+ethmacc@users.noreply.github.com>
2024-12-19 21:28:30 +0000

    Edit map colours, add control panel

--- Website/src/main.js
+++ Website/src/main.js
@@ -19,84 +19,68 @@ async function main () {
   //wait until map is loaded before loading data
   await map.once('load');
 
-  //set lsoa geojson
-  const LSOA21 = '../lsoa21.geojson'
-
+  const colorArea = '../data_processing/bikeTheftDataWithGeometry(color).geojson'
+  //still working, working on data processing
+  
   const deckOverlay = new MapboxOverlay({
       interleaved: true,
       layers: [
           new GeoJsonLayer({
-              id: 'lsoa-map', 
-              data: LSOA21, 
-              // Styles
-              stroked: true,
-              filled: false,
+              id: 'colorArea',
+              data: colorArea, 
+              stroked: true, 
+              filled: true,
+              pickable: true,
               lineWidthMinPixels: 1,
-              opacity: 0.5,
-              getLineColor: [252, 148, 3],
-              beforeId: 'place_suburbs' 
+              opacity: 0.3,
+              getLineColor: [255, 255, 255],
+              getFillColor: d => {
+                if (d.properties && d.properties.color) {
+                  const color = d.properties.color;
+                  const match = color.match(/\d+/g); 
+                  if (match) {
+                    const rgba = match.map(Number); 
+                    rgba.push(200); 
+                    return rgba;
+                  }
+                }
+                return [255, 255, 255,200];
+              },
+              beforeId: 'place_suburbs',
+              // onHover: info => {
+              //   const {coordinate,object} = info;
+              //   if(object){map.getCanvas().style.cursor = 'pointer';}
+              //   else{map.getCanvas().style.cursor = 'grab';}
+              // },
+              onClick: (info) => {
+                const {coordinate, object} = info;
+                if (object) {
+                  const properties = object.properties || {};
+                  const description = `
+                    <div>
+                      <h5>SafeBike Information</h5>
+                      <p><strong>LSOA Name:</strong> ${properties['LSOA Name']}</p>
+                      <p><strong>LSOA Code:</strong> ${properties['LSOA Code']}</p>
+                      <p><strong>Total Cases:</strong> ${properties['Total']}</p>
+                    </div>`;
+      
+                //MapLibre Popup
+                  const popup = document.getElementsByClassName('maplibregl-popup');
+                  if ( popup.length ) {popup[0].remove();} //remove previous popup on open
+      
+                  new Popup({closeOnClick: false, closeOnMove:true})
+                      .setLngLat(coordinate)
+                      .setHTML(description)
+                      .addTo(map);
+                  } else {
+                    console.log('No feature clicked.');
+                  }
+              },
             }),
       ]
   });
 
   map.addControl(deckOverlay);
-
-  const colorArea = '../data_processing/bikeTheftDataWithGeometry(color).geojson'
-  //still working, working on data processing
-  deckOverlay.setProps({
-    layers: [
-      ...deckOverlay._props.layers,
-      new GeoJsonLayer({
-        id: 'colorArea',
-        data: colorArea, 
-        stroked: true, 
-        filled: true,
-        pickable: true,
-        lineWidthMinPixels: 2,
-        opacity: 0.7,
-        getFillColor: d => {
-          if (d.properties && d.properties.color) {
-            const color = d.properties.color;
-            const match = color.match(/\d+/g); 
-            if (match) {
-              const rgba = match.map(Number); 
-              rgba.push(200); 
-              return rgba;
-            }
-          }
-          return [255, 255, 255,200];
-        },
-        beforeId: 'place_suburbs',
-        // onHover: info => {
-        //   const {coordinate,object} = info;
-        //   if(object){map.getCanvas().style.cursor = 'pointer';}
-        //   else{map.getCanvas().style.cursor = 'grab';}
-        // },
-        onClick: (info) => {
-          const {coordinate, object} = info;
-          if (object) {
-            const properties = object.properties || {};
-            const description = `
-              <div>
-                <h5>SafeBike Information</h5>
-                <p><strong>LSOA Name:</strong> ${properties['LSOA Name']}</p>
-                <p><strong>LSOA Code:</strong> ${properties['LSOA Code']}</p>
-                <p><strong>Total Cases:</strong> ${properties['Total']}</p>
-              </div>`;
-
-          //MapLibre Popup
-            new Popup({closeOnClick: false, closeOnMove:true})
-                .setLngLat(coordinate)
-                .setHTML(description)
-                .addTo(map);
-            } else {
-              console.log('No feature clicked.');
-            }
-        },
-      }),
-      
-    ]
-  })
 }
 
 main();
